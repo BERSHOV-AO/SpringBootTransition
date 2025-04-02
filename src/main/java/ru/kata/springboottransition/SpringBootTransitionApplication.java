69ad6c63ceca7d3758eb16ea7cd872b1107a46c3
Aleksey Bershov <bershov.aleksey@bk.ru>
2025-04-02 09:19:20 +0300

    Initial commit

---
+++ src/main/java/ru/kata/springboottransition/SpringBootTransitionApplication.java
@@ -0,0 +1,13 @@
+package ru.kata.springboottransition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTransitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTransitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kata/springboottransition/SpringBootTransitionApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kata.springboottransition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTransitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
